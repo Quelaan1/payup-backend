5ce2a3a63a7154cd94776f16de879b89de820c42
rahulprakash11 <rahulprakash11@gmail.com>
2024-02-10 11:07:37 +0530

    add .gitignore

--- routes/auth/otp.py
+++ routes/auth/otp.py
@@ -72,3 +72,37 @@ async def verify_otp_endpoint(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="OTP verification failed",
         )
+
+
+@router.post("/auth/verify-pan", response_model=OTPVerifyResponseSchema)
+async def verify_otp_endpoint(
+    otp_verify: OTPVerifyRequestSchema,
+):
+    try:
+        verification = client.verify.v2.services(
+            service_sid
+        ).verification_checks.create(
+            to="+91" + otp_verify.phone_number, code=otp_verify.otp
+        )
+    except TwilioRestException as twilio_error:
+        raise HTTPException(
+            status_code=twilio_error.status,
+            detail=twilio_error.msg,
+        )
+
+    if verification.status == "approved":
+
+        return OTPVerifyResponseSchema(message="OTP verified successfully")
+    elif verification.status == "pending":
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="OTP verification failed",
+        )
+
+
+# curl --request GET \
+#  --url 'https://api.sandbox.co.in/pans/binpt2390c/verify?consent=y&reason=For%20KYC%20of%20User' \
+#  --header 'Authorization: eyJhbGciOiJIUzUxMiJ9.eyJhdWQiOiJBUEkiLCJyZWZyZXNoX3Rva2VuIjoiZXlKaGJHY2lPaUpJVXpVeE1pSjkuZXlKaGRXUWlPaUpCVUVraUxDSnpkV0lpT2lKeVlXaDFiSEJ5WVd0aGMyZ3hNVUJuYldGcGJDNWpiMjBpTENKaGNHbGZhMlY1SWpvaWEyVjVYMnhwZG1WZmJFRTFVVFZhZG1oT1YxSnpORVpSVTNacmNIWnJObTVKT1c5UVMzcE1Ra1FpTENKcGMzTWlPaUpoY0drdWMyRnVaR0p2ZUM1amJ5NXBiaUlzSW1WNGNDSTZNVGN6T1RFMk5UUXpPU3dpYVc1MFpXNTBJam9pVWtWR1VrVlRTRjlVVDB0RlRpSXNJbWxoZENJNk1UY3dOelUwTXpBek9YMC5MX1piUjM3Z2Q1bTB4OXlVSG03UzRRSEtNTTJ3anByY09iTTNlWng3bjVURUNFQjFnZ0F0SHhKV3lkYUdZek9aODlVT0pnclMxcWwtMC1WTnB6VnZXQSIsInN1YiI6InJhaHVscHJha2FzaDExQGdtYWlsLmNvbSIsImFwaV9rZXkiOiJrZXlfbGl2ZV9sQTVRNVp2aE5XUnM0RlFTdmtwdms2bkk5b1BLekxCRCIsImlzcyI6ImFwaS5zYW5kYm94LmNvLmluIiwiZXhwIjoxNzA3NjI5NDM5LCJpbnRlbnQiOiJBQ0NFU1NfVE9LRU4iLCJpYXQiOjE3MDc1NDMwMzl9.obvkII7VanJ2PImqqJNUMXBblYLw_CXcuxosvO3d0uM7R93Da-tnjGHcfRNthjO0iBCWuFsKkHK_j_gFjyTkig' \
+#  --header 'accept: application/json' \
+#  --header 'x-api-key: key_live_lA5Q5ZvhNWRs4FQSvkpvk6nI9oPKzLBD' \
+#  --header 'x-api-version: 1.0'
