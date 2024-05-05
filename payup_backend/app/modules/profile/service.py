0b941064a2c7463e7888c5149622aceb4eec5e94
rahulprakash11 <rahulprakash11@gmail.com>
2024-05-05 17:42:04 +0530

    debug profile update error

--- payup_backend/app/modules/profile/service.py
+++ payup_backend/app/modules/profile/service.py
@@ -4,6 +4,7 @@ import logging
 from uuid import UUID
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import IntegrityError
 from fastapi import HTTPException, status
 
 from .model import ProfileUpdate
@@ -44,7 +45,6 @@ class ProfileService:
             obj_id {UUID} -- The profile's unique ID.
         """
         try:
-
             async with self.sessionmaker() as session:
                 user = await self._repo.get_obj(session=session, obj_id=obj_id)
                 await session.commit()
@@ -72,9 +72,42 @@ class ProfileService:
         Returns:
             List -- A list of dictionaries containing user data.
         """
-        async with self.sessionmaker() as session:
-            user = await self._repo.update_obj(
-                session=session, obj_id=obj_id, p_model=update_model
-            )
-            await session.commit()
-        return user
+
+        try:
+            async with self.sessionmaker() as session:
+                user = await self._repo.update_obj(
+                    session=session, obj_id=obj_id, p_model=update_model
+                )
+                await session.commit()
+            if not user:
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST, detail="Profile not found"
+                )
+            return user
+        except HTTPException as e:
+            raise e
+        except IntegrityError as e:
+            # Extract the details of the error
+            error_message = str(e)
+            logger.error("Error message: %s", error_message)
+
+            # Check if the error message indicates a unique constraint violation for "profiles_email_key"
+            if "profiles_email_key" in error_message.lower():
+                # Raise an HTTPException with proper context for a duplicate email
+                raise HTTPException(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    detail="Email already exists",
+                ) from e
+            # Handle other types of IntegrityErrors
+            logger.error("Other IntegrityError occurred")
+            # Handle the specific case here
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Other IntegrityError occurred",
+            ) from e
+
+        except Exception as err:
+            logger.error("error : %s", err)
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=err.args[0]
+            ) from err
